36cd7a6bf0c5644f0a7394f117222cda2bb356f9
ZahroNurrobbyAshari <zahronurrobbya@gmail.com>
2022-12-22 21:15:21 +0700

    constant

---
+++ 7-constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	const(
+		firstName string = "kepo"
+		lastName = "uhuy"
+		value = 200
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+
+
+}
