d0a65853c0de1f893173e06c5a56f7909104de3a
ZahroNurrobbyAshari <zahronurrobbya@gmail.com>
2022-12-22 21:00:41 +0700

    tipe data string

---
+++ 5-tipe-data-string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("string 1")
+	fmt.Println("string 2")
+	fmt.Println("string 3")
+	fmt.Println(len("String 4"))
+	fmt.Println("String 6"[6])
+}
