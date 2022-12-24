2e85061717211fb735e67d18fdefec19099dd2bc
ZahroNurrobbyAshari <zahronurrobbya@gmail.com>
2022-12-24 19:43:31 +0700

    operasi matematika

---
+++ 10-operasi-matematika.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var result = 10 + 10
+	fmt.Println(result)
+
+	var a = 10
+	var b = 11
+	var c = a * b
+	fmt.Println(c)
+
+	// augmented
+
+	var i = 10
+	i += 10
+	fmt.Println(i)
+
+	//unary
+	i++
+	fmt.Println(i)
+
+}
